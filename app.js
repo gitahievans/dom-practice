f7663b85eb58de6b37128a1cc98a183356841e1d
gitahievans <evanygk@gmail.com>
2022-09-19 23:31:46 +0300

    Added manipulation to the cart buttton

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const blackColor = document.querySelector(".black");
 const cartButton = document.querySelector("#button");
 const itemTag = document.querySelector(".tag");
 const imageCard = document.querySelector(".product-image");
+const feedBackBtn = document.querySelector(".feedback");
 
 redColor.addEventListener("click", () => {
   itemTag.style.backgroundColor = "red";
@@ -25,7 +26,14 @@ blackColor.addEventListener("click", () => {
 // <<-commit->>
 
 // 2. Implement the Cart Button
-cartButton.addEventListener("click", () => {});
+// We 1st create a fucntion that hides the cart button and displays the feedback button. We will then pass that fucntion to the cart eventlistener as the 2nd parameter (handler).
+const hideCartShowFeedback = () => {
+  cartButton.style.display = "none";
+  feedBackBtn.style.display = "block";
+};
+
+cartButton.addEventListener("click", hideCartShowFeedback);
+
 // <<-commit->>
 // 3. Implement the Feedback Button
 
